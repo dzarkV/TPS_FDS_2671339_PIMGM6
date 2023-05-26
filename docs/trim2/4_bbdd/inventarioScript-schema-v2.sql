c012aec877f8bd327cdb4d7b1abc87338939ba50
dzarkV <dfvargasp@unal.edu.co>
2023-05-25 20:49:41 -0500

    bbdd script de creación con unsigned y ajustes tipos de datos

---
+++ docs/trim2/4_bbdd/inventarioScript-schema-v2.sql
@@ -0,0 +1,179 @@
+-- ALGUNAS NOTAS DE TIPOS DE DATOS
+-- tinyint -128 a 127					unsigned (sin signo negativo):  0 hasta 255
+-- smallint -32.768 a 32.767			unsigned: 						0 hasta 65.535
+-- mediumint -8.388.608 a 8.388.607 	unsigned: 						0 hasta 16.777.215
+-- int -2.147.483.648 a 2.147.483.647 	unsigned:						0 hasta 4.294.967.295
+
+
+
+
+
+DROP SCHEMA IF EXISTS BBDD_INVENTARIO;
+-- -----------------------------------------------------
+-- ESTRUCTURA BBDD_INVENTARIO
+-- -----------------------------------------------------
+CREATE SCHEMA BBDD_INVENTARIO DEFAULT CHARACTER SET utf8 ;
+USE BBDD_INVENTARIO;
+
+-- -----------------------------------------------------
+-- TABLA ROLES
+-- -----------------------------------------------------
+create table ROLES ( 
+	idRol tinyint unsigned auto_increment not null,
+	nombre_rol VARCHAR(20) NOT NULL,
+    PRIMARY KEY (idRol)
+) ENGINE = InnoDB;
+
+-- -----------------------------------------------------
+-- TABLA USUARIOS
+-- -----------------------------------------------------
+create table USUARIOS (
+	idUsuario smallint unsigned auto_increment not null,
+	idRol tinyint unsigned not null,
+    nombreUsuario varchar(20) not null,
+    apellidoUsuario varchar(20) not null,
+    fechaRegistro date,
+	PRIMARY KEY (idUsuario),
+    CONSTRAINT fk_usuarios_roles
+	    FOREIGN KEY (idRol)
+	    REFERENCES ROLES (idRol)
+	    ON DELETE CASCADE
+	    ON UPDATE CASCADE
+) ENGINE = InnoDB;
+
+-- -----------------------------------------------------
+-- TABLA CREDENCIALES
+-- -----------------------------------------------------
+create table CREDENCIALES (
+	idCredenciales smallint unsigned auto_increment not null,
+	idUsuario smallint unsigned not null,
+	fechaIngreso  date,
+	contraseNa varchar (20) not null,
+	estado boolean,
+	PRIMARY KEY (idCredenciales),
+	CONSTRAINT fk_credenciales_usuarios
+	    FOREIGN KEY (idUsuario)
+	    REFERENCES USUARIOS (idUsuario)
+	    ON DELETE cascade
+	    ON UPDATE CASCADE
+) ENGINE = InnoDB;
+
+-- -----------------------------------------------------
+-- TABLA ENTRADAS
+-- -----------------------------------------------------
+create table ENTRADAS (
+	idEntrada int unsigned auto_increment not null,
+	idUsuario smallint unsigned not null,
+	fechaEntrada date not null,
+	cantidadItemsEntrada int not null,
+	costoTotalEntrada int not null,
+	costoUnitario double,
+	primary key (idEntrada),
+	constraint fk_entradas_usuarios
+	    foreign key  (idUsuario)
+	    references USUARIOS (idUsuario)
+	    on update cascade 		-- NO hay delete cascade cuando se borre un usuario, porque las entradas deben mantenerse
+) engine = InnoDB;
+
+-- -----------------------------------------------------
+-- TABLA CATEGORIAS
+-- -----------------------------------------------------
+CREATE table CATEGORIAS (
+	idCategoria tinyint unsigned auto_increment not null,
+	nombreCategoria varchar(20) not null,
+	PRIMARY KEY (idCategoria)
+) ENGINE = InnoDB;
+
+-- -----------------------------------------------------
+-- TABLA PRODUCTOS
+-- -----------------------------------------------------
+create table PRODUCTOS (
+	idProducto int unsigned auto_increment not null,
+	idEntrada int unsigned not null,
+	idCategoria tinyint unsigned not null,
+	nombreProducto varchar(20) not null,
+	precioProducto double not null,
+	descripcionProducto varchar(100) NOT NULL,
+	primary key(idProducto),
+	constraint fk_productos_entradas
+	    foreign key  (idEntrada)
+	    references ENTRADAS (idEntrada)
+	    ON DELETE CASCADE
+	    ON UPDATE CASCADE,
+	constraint fk_productos_categorias
+	    foreign key (idCategoria)
+	    references CATEGORIAS (idCategoria)
+) engine = InnoDB;
+
+-- -----------------------------------------------------
+-- TABLA PROVEEDORES
+-- -----------------------------------------------------
+create table PROVEEDORES (
+	idProveedor smallint unsigned auto_increment not null,
+    idEntrada int unsigned not null,
+    empresa varchar(50) not null,
+    direccionProveedor varchar(100),
+    nombreProveedor varchar(100) not null,
+    telefonoProveedor varchar(15) not null,
+    emailProveedor varchar(50),
+    descripcionProveedor varchar(150),
+    primary key(idProveedor),
+    constraint fk_proveedores_entradas
+	    foreign key (idEntrada)
+	    references ENTRADAS (idEntrada)
+	    ON DELETE cascade
+	    ON UPDATE CASCADE
+) engine = InnoDB;	
+
+-- -----------------------------------------------------
+-- TABLA VENTAS
+-- -----------------------------------------------------
+create table VENTAS (
+	idVenta int unsigned auto_increment not null,
+	idProducto int unsigned not null,
+	fechaVenta datetime not null,
+	cantidadItemsVentaByProducto int not null,
+	valotTotalVenta int not null,
+	primary key (idVenta),
+	constraint fk_ventas_productos
+		foreign key (idProducto)
+		references PRODUCTOS (idProducto)
+		on update cascade
+) engine = InnoDB;
+
+-- -----------------------------------------------------
+-- TABLA FACTURAS
+-- -----------------------------------------------------
+create table FACTURAS (
+	idFactura int unsigned auto_increment not null,
+	idVenta int unsigned not null,
+	nitEmpresaFactura varchar(50) not null,
+	cantidadProductos int not null,
+	ivaProducto int default 19 not null,
+	direccionEmpresaFactura varchar(100),
+	razonSocialFactura varchar(50),
+	fechaFactura datetime not null,
+	valorTotalFactura double not null,
+	primary key (idFactura),
+	constraint fk_facturas_ventas
+		foreign key (idVenta)
+		references VENTAS (idVenta)
+		on update cascade
+) engine =InnoDB;
+
+-- -----------------------------------------------------
+-- TABLA GASTOS
+-- -----------------------------------------------------
+create table GASTOS (
+	idGasto tinyint unsigned auto_increment not null,
+	idUsuario smallint unsigned not null,
+	tipoGasto varchar(50) not null,
+	valorGasto double not null,
+	primary key (idGasto),
+	constraint fk_gastos_usuario
+		foreign key (idUsuario)
+		references USUARIOS (idUsuario)
+		on update cascade
+) engine = InnoDB;
+
+
