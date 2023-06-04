3cf7f0b50cc7c8c781ca3b560a77041361c0027d
dzarkV <dfvargasp@unal.edu.co>
2023-06-04 08:36:14 -0500

    inicio backend modulo usuarios

---
+++ backend/users-module/config/db.py
@@ -0,0 +1,10 @@
+import pymongo
+from dotenv import load_dotenv
+from os import getenv
+
+load_dotenv()
+config = getenv("MONGO_DB_STRING")
+conn = pymongo.MongoClient(config)
+
+db = conn[getenv("DB_NAME")]
+collection = db.get_collection(getenv("COLLECTION_NAME"))
