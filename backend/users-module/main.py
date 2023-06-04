cff5f1b6a8ffd529f9634babf21e54cd2c656e95
Diego Vargas <91356068+dzarkV@users.noreply.github.com>
2023-06-04 17:58:40 +0000

    provisional de autenticacion con token

--- backend/users-module/main.py
+++ backend/users-module/main.py
@@ -1,23 +1,86 @@
-from fastapi import Depends, FastAPI
-# parte de token con OAuth2
 from typing import Annotated
-from fastapi.security import OAuth2PasswordBearer
-from models.usuario import Usuario
-from models.rol import Rol
 
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+# from models.usuario import Usuario
+from models.usuario import User
+# from models.rol import Rol
 
 
 app = FastAPI(title='Users module', 
     description='API REST de servicio de usuarios del sistema MGM', 
     version='0.0.1')
 
-
 @app.get('/')
 def root_page():
-    return {'message': 'Qué hace, socio'}
+    return {'message': 'Qué hace'}
 
-oauth_scheme = OAuth2PasswordBearer(tokenUrl='/login')
+oauth_scheme = OAuth2PasswordBearer(tokenUrl='/token')
 
-@app.get('/login')
-def login(token: Annotated[str, Depends(oauth_scheme)]):
-    return {'message': 'Login: ' + token}
+#DB
+fake_users_db = {
+    "dinensep": {
+        "username": "dinensep",
+        "full_name": "Dinense Pardo",
+        "email": "segundop@sistemamgm.com",
+        "password": "12345",
+        "disabled": False,
+    },
+    "segundop": {
+        "username": "segundop",
+        "full_name": "Segundo Fontecha",
+        "email": "alice@sistemamgm.com",
+        "password": "12345",
+        "disabled": True,
+    }
+}
+
+# def fake_hash_password(password: str):
+#     return "fakehashed" + password
+
+def get_user(db, username: str):
+    if username in db:
+        return db[username]
+
+# def fake_decode_token(token):
+#     # This doesn't provide any security at all
+#     # Check the next version
+#     user = get_user(fake_users_db, token)
+#     return user
+
+async def get_current_user(token: Annotated[str, Depends(oauth_scheme)]):
+    user = get_user(fake_users_db, token)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Credenciales de autenticación inválidas",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return user
+
+async def get_current_active_user(
+    current_user: Annotated[User, Depends(get_current_user)]
+):
+    if current_user["disabled"]:
+        raise HTTPException(status_code=400, detail="usuario inactivo")
+    return current_user
+
+@app.post("/token")
+async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    user = fake_users_db.get(form_data.username)
+    flat_password = form_data.password
+    print(user)
+    if not user:
+        raise HTTPException(status_code=400, detail="Nombre de usuario o contaseña incorrectos")
+    # user = UserInDB(**user_dict)
+    # hashed_password = fake_hash_password(form_data.password)
+    if not flat_password == user["password"]:
+        raise HTTPException(status_code=400, detail="Nombre de usuario o contaseña incorrectos")
+
+    return {"access_token": user["username"], "token_type": "bearer"}
+
+@app.get("/users/me")
+async def read_users_me(
+    current_user: Annotated[User, Depends(get_current_active_user)]
+):
+    return current_user
--- backend/users-module/models/usuario.py
+++ backend/users-module/models/usuario.py
@@ -2,9 +2,15 @@ from pydantic import BaseModel
 # from typing import Optional
 from datetime import date
 
-class Usuario(BaseModel):
-    id_usuario: int
-    nombre_usuario: str
-    apellido_usuario: str
-    fecha_registro: date = date.today()
-    rol_usuario: int
+# class Usuario(BaseModel):
+#     id_usuario: int
+#     nombre_usuario: str
+#     apellido_usuario: str
+#     fecha_registro: date = date.today()
+#     rol_usuario: int | None = None
+class User(BaseModel):
+    username: str
+    full_name: str
+    email: str | None = None
+    password: str | None = None
+    disabled: bool | None = None
