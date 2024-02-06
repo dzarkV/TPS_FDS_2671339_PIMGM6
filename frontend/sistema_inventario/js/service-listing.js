88328addd4e337000e7d06ec79d01a400ec663dc
dzarkV <dfvargasp@unal.edu.co>
2024-02-06 17:23:42 -0500

    cambiar estado a usuarios completado

--- frontend/sistema_inventario/js/service-listing.js
+++ frontend/sistema_inventario/js/service-listing.js
@@ -17,7 +17,7 @@ function buscarUsuario() {
   // Metodo que pinta los datos
   const mostrarData = (data) => {
     let body = "";
-    body = `<tr><td>${data.nombre_usuario}</td><td>${data.apellido_usuario}</td><td>${data.fecha_registro}</td></tr>`;
+    body = `<tr><td>${data.nombre_usuario}</td><td>${data.apellido_usuario}</td><td>${data.fecha_registro}</td><td>${data.credenciales.usuario}</td></tr>`;
     document.getElementById("data").innerHTML = body;
   };
 }
@@ -42,7 +42,7 @@ function listarUsuarios() {
     let body = "";
     // Si la pagina actual es consultar_usuario.html
     if(location.pathname.endsWith("consultar_usuario.html")== true){
-      // Datos de usuario en la tabla 
+      // Añadir datos de usuario en la tabla 
       for (var i = 0; i < data.length; i++) {
         body += `<tr><td>${data[i].nombre_usuario}</td><td>${data[i].apellido_usuario}</td><td>${data[i].fecha_registro}</td><td>${data[i].credenciales.usuario}</td></tr>`;
       }
@@ -50,14 +50,14 @@ function listarUsuarios() {
     }
     // Si la pagina actual es estados_de_usuarios.html
     else if(location.pathname.endsWith("estados_de_usuarios.html")== true){
-      // Datos de usuario en la tabla
+      // Añdir datos de usuario en el body de la tabla
       for (var i = 0; i < data.length; i++) {
-        body += `<tr><td>${data[i].nombre_usuario}</td><td>${data[i].apellido_usuario}</td><td>${data[i].rol_usuario.nombre_rol}</td>`;
+        body += `<tr data-id="${data[i].id_usuario}" data-nombre="${data[i].nombre_usuario}" data-user=${data[i].credenciales.usuario}><td>${data[i].nombre_usuario}</td><td>${data[i].apellido_usuario}</td><td>${data[i].rol_usuario.nombre_rol}</td>`;
         if(data[i].credenciales.estado == true){
           // Tambien se agrega un boton para cambiar el estado del usuario
-          body += `<td><a class="estilo-intro-icon icon-user" style="color:green;"></a></td><td><button id="btn-state" class="btn btn-primary" style="background-color: red;" onclick="cambiarEstadoUsuario()">Deshabilitar</button></td></tr>`;
+          body += `<td><a class="estilo-intro-icon icon-user" style="color:green;" title="Activo"></a></td><td><button id="btn-state" class="btn btn-primary" style="background-color: red;" onclick="cambiarEstadoUsuario()">Deshabilitar</button></td></tr>`;
         } else {
-          body += `<td><a class="estilo-intro-icon icon-user" style="color:red;"></a></td><td><button id="btn-state" class="btn btn-primary" onclick="cambiarEstadoUsuario()">Habilitar</button></td></tr>`;
+          body += `<td><a class="estilo-intro-icon icon-user" style="color:red;" title="No activo"></a></td><td><button id="btn-state" class="btn btn-primary" onclick="cambiarEstadoUsuario()">Habilitar</button></td></tr>`;
         }
       }
       document.getElementById("data-users").innerHTML = body;
@@ -66,39 +66,70 @@ function listarUsuarios() {
 }
 
 function cambiarEstadoUsuario() {
-  // Consumir el endpoint usuario service para buscar un usuario por nombre y obtener id
-  var urlBuscar = "https://sistema-mgm-service-users.azurewebsites.net/api/usuario/name?value=";
-  const jwtToken = getTokenFromLocalStorage();
-  const idUsuario = '';
+  // Cambiar el estado del usuario
+  
+  // Obtener todos los botones de estado
+  const botonesEstado = document.querySelectorAll("#btn-state");
 
-  let nombreUsuario = '';
-  let row = document.querySelector(this.button).closest("tr");
-  nombreUsuario = row.querySelector("td:first-child").textContent;
-  console.log(nombreUsuario);
+  // Agregar un evento a cada boton
+  botonesEstado.forEach(function(boton) {
+    boton.addEventListener("click", function(event) {
+      // Obtener los datos de la fila del boton seleccionado
+      const accion = boton.textContent || boton.innerText || boton.innerHTML;
+      const fila = event.target.closest('tr');
+      const idUsuario = fila.getAttribute('data-id');
+      const nombreUsuario = fila.getAttribute('data-user');
+      // Llamar a la funcion para cambiar el estado del usuario
+      actualizarEstadoUsuario(accion, idUsuario, nombreUsuario);
+    });
+  });
+}
+
+function actualizarEstadoUsuario(accion, id, usuario) {
+  // Actualizar el estado del usuario
+  const status = accion === "Habilitar" ? true : false;
+  // Consumir el endpoint usuario service para buscar un usuario por nombre y obtener id
+  var urlBuscar = "https://sistema-mgm-service-users.azurewebsites.net/api/usuario/username?value=";
+  const jwtToken = getTokenFromLocalStorage();
 
   // Consultar API pasando el nombre buscado
-  fetch(urlBuscar + nombreUsuario, {
+  fetch(urlBuscar + usuario, {
     method: "GET",
     headers: {
       Authorization: `Bearer ${jwtToken}`,
     },
   })
-    .then((response) => response.json())
-    .then((datos) => idUsuario = datos.data[0].id_usuario)
-    .then((idUsuario) => console.log(idUsuario))
+    .then(async (response) => {
+      const user = await response.json();
+      // Se procede a actualizar el estado del usuario
+      url = "https://sistema-mgm-service-users.azurewebsites.net/api/usuario/";
+      fetch(url + id, {
+        method: "PUT",
+        headers: {
+          accept: "application/json",
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${jwtToken}`,
+        },
+        body: JSON.stringify({
+          nombre_usuario: user.data[0].nombre_usuario,
+          apellido_usuario: user.data[0].apellido_usuario,
+          credenciales: {
+            estado: status,
+          },
+        }),
+      })
+        .then((responseUpdatedStatus) => {
+          if (responseUpdatedStatus.status === 200) {
+            alert(`Usuario ${user.data[0].nombre_usuario} ${user.data[0].apellido_usuario} ${status ? "habilitado" : "deshabilitado"} exitosamente.`);
+            location.reload();
+          } else {
+            alert("Error al actualizar el estado del usuario");
+          }
+        })
+        .catch((error) => console.log(error));
+    })
     .catch((error) => console.log(error));
   
-  // var urlActualizar = "https://sistema-mgm-service-users.azurewebsites.net/api/usuario/";
-  // // Consultar API pasando el nombre buscado con token
-  // fetch(urlActualizar + idUsuario, {
-  //   method: "PUT",
-  //   headers: {
-  //     Authorization: `Bearer ${jwtToken}`,
-  //   },
-  // })
-  //   .then((response) => response.json())
-  //   .then((datos) => console.log(datos))
-  //   .catch((error) => console.log(error));
 }
 
 function getTokenFromLocalStorage() {
